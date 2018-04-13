39b341a87de08e29e2395be1a3a9d88b6dff8bd5
Felipe Peternella <felipevex@gmail.com>
2018-04-13 12:31:59 -0300

    agora foi erro meu

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -36,11 +36,11 @@ var PLATFORM = {
     },
     ANDROID: {
         dest: [
-            ANDROID_DIR + '/google-services.json'
+            ANDROID_DIR + '/google-services.json',
+			ANDROID_DIR + '/app/google-services.json'
         ],
         src: [
             'google-services.json',
-			ANDROID_DIR + '/app/google-services.json',
             ANDROID_DIR + '/assets/www/google-services.json',
             'www/google-services.json'
         ],
